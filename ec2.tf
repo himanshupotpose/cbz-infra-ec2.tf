9a5bea757cca5c62d661a8b6f9e7597852ddcd87
Himanshu <himanshupotpose388@gmail.com>
2026-03-18 18:12:28 +0530

    fix again

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2_instance" {
     ami = "ami-05d2d839d4f73aafb"
     instance_type = "t3.micro"
     key_name = "dell"
-    vpc_security_group_ids = ["sg-03b15ce3f19883c15"]
+    vpc_security_group_ids = ["sg-0843a7cb09cfe1125"]
     tags = {
         Name = "my_ec2_instance"
         env = "dev"
